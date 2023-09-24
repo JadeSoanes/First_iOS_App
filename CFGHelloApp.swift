4c08e0a69686851d05ac4d809c2467afccc1f437
JadeSoanes <111527331+JadeSoanes@users.noreply.github.com>
2023-09-24 14:22:47 +0100

    Add files via upload

---
+++ CFGHelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFGHelloApp.swift
+//  CFGHello
+//
+//  Created by Jade Soanes on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CFGHelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
